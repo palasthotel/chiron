431b33094256cbf0fd5a6596945ce8df665002e7
Ben <benjamin@birkenhake.org>
2014-01-15 10:47:45 +0100

    Added Base for Drupal and Wordpress Wrappers

---
+++ wordpress/chiron-plugin-wrapper.php
@@ -0,0 +1,6 @@
+<?php
+
+// This File contains all necessary Wordpress-Plugin-Wrapper Functions.
+
+
+?>
---
+++ wordpress_plugin.php
@@ -0,0 +1,8 @@
+<?php
+/**
+* Plugin Name: Chiron
+* Description: The Teacher of Heros
+* Version: 0.1
+* Author: Palasthotel (in Person: Benjamin Birkenhake, Enno Welbers)
+* Author URI: http://www.palasthotel.de
+*/
