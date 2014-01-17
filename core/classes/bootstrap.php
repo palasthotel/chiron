ee01e98664958f0c714ba0ce08de349bbbbd25b5
Ben <benjamin@birkenhake.org>
2014-01-17 14:53:53 +0100

    Removed Session Start from bootstrap

--- core/classes/bootstrap.php
+++ core/classes/bootstrap.php
@@ -4,11 +4,10 @@ error_reporting(E_ERROR | E_WARNING | E_PARSE);
 include 'config.php';
 include 'simplepie.inc';
 include 'chiron.php';
-include 'feed.php';
+include 'source.php';
 include 'item.php';
 include 'user.php';
 include 'public.php';
 include 'private.php';
 
 $chiron = new chiron();
-session_start();
